7e4aeb94656452cd09e85f928cd0351f79989170
OtakuCoin <otakucoin@otaku-coin.com>
2018-09-26 18:48:04 +0900

    Fix bytes argument warnings about keccak256

--- contracts/token/ERC865/ERC865Token.sol
+++ contracts/token/ERC865/ERC865Token.sol
@@ -233,7 +233,7 @@ contract ERC865Token is ERC865, StandardToken {
         returns (bytes32)
     {
         /* "48664c16": transferPreSignedHashing(address,address,address,uint256,uint256,uint256) */
-        return keccak256(bytes4(0x48664c16), _token, _to, _value, _fee, _nonce);
+        return keccak256(abi.encodePacked(bytes4(0x48664c16), _token, _to, _value, _fee, _nonce));
     }
 
     /**
@@ -256,7 +256,7 @@ contract ERC865Token is ERC865, StandardToken {
         returns (bytes32)
     {
         /* "f7ac9c2e": approvePreSignedHashing(address,address,uint256,uint256,uint256) */
-        return keccak256(bytes4(0xf7ac9c2e), _token, _spender, _value, _fee, _nonce);
+        return keccak256(abi.encodePacked(bytes4(0xf7ac9c2e), _token, _spender, _value, _fee, _nonce));
     }
 
     /**
@@ -279,7 +279,7 @@ contract ERC865Token is ERC865, StandardToken {
         returns (bytes32)
     {
         /* "a45f71ff": increaseApprovalPreSignedHashing(address,address,uint256,uint256,uint256) */
-        return keccak256(bytes4(0xa45f71ff), _token, _spender, _addedValue, _fee, _nonce);
+        return keccak256(abi.encodePacked(bytes4(0xa45f71ff), _token, _spender, _addedValue, _fee, _nonce));
     }
 
      /**
@@ -302,7 +302,7 @@ contract ERC865Token is ERC865, StandardToken {
         returns (bytes32)
     {
         /* "59388d78": decreaseApprovalPreSignedHashing(address,address,uint256,uint256,uint256) */
-        return keccak256(bytes4(0x59388d78), _token, _spender, _subtractedValue, _fee, _nonce);
+        return keccak256(abi.encodePacked(bytes4(0x59388d78), _token, _spender, _subtractedValue, _fee, _nonce));
     }
 
     /**
@@ -327,7 +327,7 @@ contract ERC865Token is ERC865, StandardToken {
         returns (bytes32)
     {
         /* "b7656dc5": transferFromPreSignedHashing(address,address,address,uint256,uint256,uint256) */
-        return keccak256(bytes4(0xb7656dc5), _token, _from, _to, _value, _fee, _nonce);
+        return keccak256(abi.encodePacked(bytes4(0xb7656dc5), _token, _from, _to, _value, _fee, _nonce));
     }
 
     /**
